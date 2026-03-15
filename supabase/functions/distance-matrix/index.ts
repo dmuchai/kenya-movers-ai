999cbff9121b363cefd0eed20768f436f252fca9
Dennis Muchai <dmmuchai@gmail.com>
2026-03-15 18:16:20 +0300

    Fix CORS and JWT config for places and distance-matrix edge functions

--- supabase/functions/distance-matrix/index.ts
+++ supabase/functions/distance-matrix/index.ts
@@ -21,7 +21,9 @@ export interface DistanceMatrixResponse {
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type'
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Max-Age': '86400'
 }
 
 Deno.serve(async (req) => {
