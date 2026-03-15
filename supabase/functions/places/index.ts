d894189f773a99f377568d5591333119d324ad15
Dennis Muchai <dmmuchai@gmail.com>
2026-03-15 16:45:04 +0300

    Enable public browser access for places edge function

--- supabase/functions/places/index.ts
+++ supabase/functions/places/index.ts
@@ -25,7 +25,9 @@ type PlacesRequest = AutocompleteRequest | PlaceDetailsRequest
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type'
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Max-Age': '86400'
 }
 
 const jsonHeaders = { ...corsHeaders, 'Content-Type': 'application/json' }
